c12d925515f738facf72c4703bfb375150661724
kardash-bp <kardasbp@gmail.com>
2021-09-20 12:21:17 +0200

    clean up

--- Timer.js
+++ Timer.js
@@ -34,26 +34,10 @@ export default class Timer {
       if (this.timeInput.value > 0) {
         this.timeInput.value--
       }
-      this.animateCircle()
       console.log('in int', this.timeInput.value)
       if (this.timeInput.value <= 0) {
         clearInterval(this.intId)
       }
     }, 1000)
-  }
-  animateCircle() {
-    let radialEl = document.querySelector('.cp-wrapper')
-    let fullEls = radialEl.querySelectorAll('.cp-full, .cp-regular')
-    let fixEl = radialEl.querySelector('.cp-fix')
-    let percentage = radialEl.querySelector('.cp-percent-number')
-    percentage.innerText = this.timeInput.value
-    let degree = this.timeInput.value
-    fullEls.forEach((fullEl) => {
-      fullEl.style.transform = `rotate(${1.8 * degree}deg)`
-    })
-    fixEl.style.transform = `rotate(${1.8 * degree * 2}deg)`
-    radialEl.setAttribute('data-progress', degree)
-    percentage.innerText = `${degree}`
-    console.log(degree)
   }
 }
